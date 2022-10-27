73466234fc85cb6474da377248df597db58b2d70
Blessed Adodo <collzbadodo@gmail.com>
2022-10-26 23:49:48 -0700

    created intern html card in my page-tempalete.js

--- src/page-template.js
+++ src/page-template.js
@@ -43,4 +43,24 @@ const generateTeam = (team) => {
       </div>
     `;
   };
-  
+  //create Intern html card
+  const generateIntern = (intern) => {
+    return `
+      <div class="card employee-card shadow mb-5 rounded" style="width: 18rem;">
+        <div class="card-header bg-primary">
+          <h2 class="card-title text-light">${intern.getName()}</h2>
+          <h3 class="card-title text-light"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
+        </div>
+        <div class="container bg-light">
+          <div class="card-body">
+            <ul class="list-group list-group-flush table-bordered">
+              <li class="list-group-item">ID: ${intern.getId()}</li>
+              <li class="list-group-item">Email: 
+                <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+              <li class="list-group-item">School: ${intern.getSchool()}</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    `;
+  };
